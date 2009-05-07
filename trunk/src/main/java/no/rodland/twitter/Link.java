73c69bf695620df2b0a99abe8483781315f0f252
fmrodland <fmrodland@3c1b1a9a-3af7-11de-9b3e-8f53452658d7>
2009-05-07 11:11:02 +0000

    adding

---
+++ trunk/src/main/java/no/rodland/twitter/Link.java
@@ -0,0 +1,84 @@
+package no.rodland.twitter;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: fmr
+ * Date: Apr 29, 2009
+ * Time: 8:00:52 PM
+ */
+public class Link {
+    private static Pattern MULTIPLE_URLS = Pattern.compile("(https?://.*)*(https?://.*)");
+    private static Pattern GOOGLE_ENDING = Pattern.compile("([^&]*)&.*");
+    private static final String FUNNY_HTTP = "http%3A";
+    private static final String NORMAL_HTTP = "http:";
+
+    String origLink;
+    String simplified;
+
+    public Link(String origLink) {
+        this.origLink = origLink;
+        this.simplified = simplify(origLink);
+    }
+
+    public String getLink() {
+        return simplified;
+    }
+
+    public static String simplify(String origLink) {
+        String retString = fixFunnyHttp(origLink);
+        retString = removeMulitpleUrls(retString);
+        retString = removeParams(retString);
+
+        return retString;
+    }
+
+    private static String fixFunnyHttp(String url) {
+        if (url == null) {
+            return null;
+        }
+        return url.replaceAll(Link.FUNNY_HTTP, Link.NORMAL_HTTP);
+    }
+
+    @Override
+    public String toString() {
+        return "Link: " + origLink + " simplified to " + simplified;
+    }
+
+    public static String removeMulitpleUrls(String url) {
+        if (url == null) {
+            return null;
+        }
+        Matcher m = MULTIPLE_URLS.matcher(url);
+        if (m.matches()) {
+            return m.group(2);
+        }
+        return url;
+    }
+
+    public static String removeParams(String url) {
+        if (url == null) {
+            return null;
+        }
+        Matcher m = GOOGLE_ENDING.matcher(url);
+        if (m.matches()) {
+            return m.group(1);
+        }
+        return url;
+    }
+
+
+//    m = endUrlRegexp.matcher(link);
+//
+//    if (m.matches()){
+//        String tmpLink = m.group(1);
+//        log.info(link + " replaced with " + tmpLink);
+//
+//        link = tmpLink;
+//    }
+//
+
+
+}
